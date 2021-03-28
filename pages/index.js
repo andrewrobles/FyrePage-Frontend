dda85d143f2c738abd3c485245eb3e6c6ab850e5
Andrew Robles <me@andrewrobles.com>
2021-03-28 05:25:54 -0700

    Render link button labels

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,51 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const labels = [
+    'TikTok', 
+    'Instagram', 
+    'YouTube', 
+    'LinkedIn', 
+    'Github'
+  ]
+
+  const links = [
+    'https://www.tiktok.com/@andrew.robles',
+    'https://www.instagram.com/andrewroblesdev/',
+    'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
+    'https://www.linkedin.com/in/andrew-robles-dev/',
+    'https://github.com/andrewrobles'
+  ]
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Andrew Robles</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <p>Andrew Robles</p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <p>Hello, world! I'm a web developer and content creator from California 🌴</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <LinkButtonColumn labels={labels}/>
     </div>
+  )
+}
+
+function LinkButtonColumn(props) {
+  return (
+    <span>
+      {props.labels.map(element => <LinkButton label={element}/>)}
+    </span>
+  )
+}
+
+function LinkButton(props) {
+  return (
+    <span>
+      {props.label}
+      <br/>
+    </span>
   )
 }
