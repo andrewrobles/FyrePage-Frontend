3ab9c2079ff9ea91bbd9744e7b02e530b39b832c
Andrew Robles <me@andrewrobles.com>
2021-04-05 05:31:32 -0700

    Render data from object

--- pages/index.js
+++ pages/index.js
@@ -4,39 +4,42 @@ import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Home() {
-  const labels = [
-    'TikTok', 
-    'Instagram', 
-    'YouTube', 
-    'LinkedIn', 
-    'Github'
-  ]
-
-  const links = [
-    'https://www.tiktok.com/@andrew.robles',
-    'https://www.instagram.com/andrewroblesdev/',
-    'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
-    'https://www.linkedin.com/in/andrew-robles-dev/',
-    'https://github.com/andrewrobles'
-  ]
+  const data = {
+    'name': 'Andrew Robles',
+    'image': '/profile.png',
+    'links': [
+      'https://www.tiktok.com/@andrew.robles',
+      'https://www.instagram.com/andrewroblesdev/',
+      'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
+      'https://www.linkedin.com/in/andrew-robles-dev/',
+      'https://github.com/andrewrobles'
+    ],
+    'labels': [
+      'TikTok',
+      'Instagram',
+      'YouTube',
+      'LinkedIn',
+      'Github'
+    ]
+  }
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Andrew Robles</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{data.name}</title>
+        <link rel="icon" href={data.image} />
       </Head>
 
       <Image
         className={styles.image} 
-        src='/profile.png'
+        src={data.image}
         height={105}
         width={105}
       />
 
-      <div className={styles.name}>Andrew Robles</div>
+      <div className={styles.name}>{data.name}</div>
 
-      <LinkButtonColumn labels={labels} links={links}/>
+      <LinkButtonColumn labels={data.labels} links={data.links}/>
     </div>
   )
 }
