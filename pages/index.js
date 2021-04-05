de80f8a42c27c9989b0128f894ed4a32b1f20f5d
andrewrobles <andrewroblesdev@gmail.com>
2021-04-05 12:15:13 -0700

    Update index

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,30 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Profile from '../components/profile/profile'
 
 export default function Home() {
-  const data = {
-    'name': 'Andrew Robles',
-    'image': '/profile.png',
-    'links': [
-      'https://www.tiktok.com/@andrew.robles',
-      'https://www.instagram.com/andrewroblesdev/',
-      'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
-      'https://www.linkedin.com/in/andrew-robles-dev/',
-      'https://github.com/andrewrobles'
-    ],
-    'labels': [
-      'TikTok',
-      'Instagram',
-      'YouTube',
-      'LinkedIn',
-      'Github'
-    ]
-  }
+    const andrewrobles = {
+        'name': 'Andrew Robles',
+        'image': '/andrewrobles.png',
+        'links': [
+          'https://www.tiktok.com/@andrew.robles',
+          'https://www.instagram.com/andrewroblesdev/',
+          'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
+          'https://www.linkedin.com/in/andrew-robles-dev/',
+          'https://github.com/andrewrobles'
+        ],
+        'labels': [
+          'TikTok',
+          'Instagram',
+          'YouTube',
+          'LinkedIn',
+          'Github'
+        ]
+    }
+    
+    const userData = {
+        'andrewrobles': andrewrobles,
+    }
 
   return (
-    <Profile data={data}/>
+    <Profile data={userData.andrewrobles}/>
   )
 }
