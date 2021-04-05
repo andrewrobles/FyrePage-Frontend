68b209d0c016089ecfaad08a020287b0e418d97a
Andrew Robles <me@andrewrobles.com>
2021-04-05 06:13:09 -0700

    Use props to render the data

--- pages/[user].js
+++ pages/[user].js
@@ -1,10 +1,36 @@
 import { useRouter } from 'next/router'
+import Profile from '../components/profile/profile'
 
-const Profile = () => {
+const User = () => {
+    const andrewrobles = {
+        'name': 'Andrew Robles',
+        'image': '/profile.png',
+        'links': [
+          'https://www.tiktok.com/@andrew.robles',
+          'https://www.instagram.com/andrewroblesdev/',
+          'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
+          'https://www.linkedin.com/in/andrew-robles-dev/',
+          'https://github.com/andrewrobles'
+        ],
+        'labels': [
+          'TikTok',
+          'Instagram',
+          'YouTube',
+          'LinkedIn',
+          'Github'
+        ]
+    }
+
+    const userData = {
+        'andrewrobles': andrewrobles
+    }
+
     const router = useRouter()
     const { user } = router.query
 
-    return <p>{ user }</p>
+    return <Profile data={userData['andrewrobles']}/>
+
 }
 
-export default Profile
+
+export default User 
