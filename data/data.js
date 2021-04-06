7bc974da245bab3671c344259ebb2cb5f37c97f9
andrew.robles <andrewroblesdev@gmail.com>
2021-04-06 13:51:19 -0700

    Add config data file

---
+++ data/data.js
@@ -0,0 +1,24 @@
+const andrewrobles = {
+    'name': 'Andrew Robles',
+    'image': '/andrewrobles.png',
+    'links': [
+        'https://www.tiktok.com/@andrew.robles',
+        'https://www.instagram.com/andrewroblesdev/',
+        'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
+        'https://www.linkedin.com/in/andrew-robles-dev/',
+        'https://github.com/andrewrobles'
+    ],
+    'labels': [
+        'TikTok',
+        'Instagram',
+        'YouTube',
+        'LinkedIn',
+        'Github'
+    ]
+}
+
+const data = {
+    'andrewrobles': andrewrobles,
+}
+
+export default data
