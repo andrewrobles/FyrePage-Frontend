1cc8f7375d0f46709a0dcb344b315c1cca48bc38
Andrew Robles <me@andrewrobles.com>
2021-04-06 05:05:58 -0700

    Reduce top padding

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Profile from '../components/profile/profile'
 
