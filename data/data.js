3ec9914461fcbe8475aa62ac7f45feb1eead7f19
Andrew Robles <me@andrewrobles.com>
2021-04-09 05:07:22 -0700

    Add user data for all users

--- data/data.js
+++ data/data.js
@@ -17,8 +17,66 @@ const andrewrobles = {
     ]
 }
 
+const holopanio = {
+    'name': 'Jackson Roberts',
+    'image': '/jackson.png',
+    'links': [
+        'https://www.tiktok.com/@holopanio',
+        'https://www.instagram.com/holopaino/',
+        'https://dogehouse.tv/user/HoloPanio',
+        'https://github.com/HoloPanio'
+    ],
+    'labels': [
+        'TikTok',
+        'Instagram',
+        'Dogehouse',
+        'Github',
+    ]
+}
+
+const erinsantamaria = {
+    'name': 'Erin Santamaria',
+    'image': '/erin.png',
+    'links': [
+        'https://www.tiktok.com/@recruiter_rin',
+        'https://www.linkedin.com/in/erin-santamaria-4604a274',
+    ],
+    'labels': [
+        'TikTok',
+        'LinkedIn',
+    ]
+} 
+
+const theekrystallee = {
+    'name': 'Krystal Lee',
+    'image': '/krystal.png',
+    'links': [
+        'https://www.youtube.com/channel/UCXVGYwQXZ18CdlrMsBpQFsw',
+        'https://open.spotify.com/episode/4x91OuTcadCUcHtCHfSr0Y',
+        'https://bitclout.com/u/theekrystallee',
+        'https://www.gofundme.com/c/act/stop-aapi-hate',
+        'https://linktr.ee/creativeasians',
+        'https://docs.google.com/forms/d/e/1FAIpQLSdsU8QGGRQhgwcJy69jMgMobwwDsOOBCvpKnYI6kPq9MLfY9Q/viewform',
+        'https://discord.com/invite/wYsNTgywF2',
+        'https://anchor.fm/uncaughtexceptions',
+    ],
+    'labels': [
+        'Uncaught Exceptions YouTube',
+        'Andrew Robles Podcast Interview',
+        'thekrystallee coin',
+        '#StopAsianHate GoFundMe',
+        'Creative Asian Community',
+        'apply to be my simp',
+        'Creators of TechTok Discord',
+        'Podcast Links'
+    ]
+}
+
 const data = {
-    'andrewrobles': andrewrobles 
+    'andrewrobles': andrewrobles,
+    'holopanio': holopanio,
+    'erinsantamaria': erinsantamaria,
+    'theekrystallee': theekrystallee
 }
 
 export default data
