f311ad4c943213feea5c352ebf7f41ed499ade14
Jackson <jrobe3221@gmail.com>
2021-04-09 12:48:31 -0500

    Split profiles into different files.

--- data/data.js
+++ data/data.js
@@ -1,116 +1,12 @@
-const andrewrobles = {
-    'name': 'Andrew Robles',
-    'image': '/andrew.png',
-    'buttons': [
-        {
-            'link': 'https://www.tiktok.com/@andrew.robles',
-            'label': 'TikTok'
-        },
-        {
-            'link': 'https://www.instagram.com/andrewroblesdev/',
-            'label': 'Instagram'
-        },
-        {
-            'link': 'https://www.youtube.com/channel/UCqNGTj5w7NxE74m25So27gg',
-            'label': 'TikTok'
-        },
-        {
-            'link': 'https://www.linkedin.com/in/andrew-robles-dev/',
-            'label': 'YouTube'
-        },
-        {
-            'link': 'https://github.com/andrewrobles',
-            'label': 'Github'
-        },
-    ],
-}
+import fs from 'fs';
 
-const holopanio = {
-    'name': 'Jackson Roberts',
-    'image': '/jackson.png',
-    'buttons': [
-        {
-            'link': 'https://www.tiktok.com/@holopanio',
-            'label': 'TikTok'
-        },
-        {
-            'link': 'https://www.instagram.com/holopaino/',
-            'label': 'Instagram'
-        },
-        {
-            'link': 'https://dogehouse.tv/user/HoloPanio',
-            'label': 'Dogehouse'
-        },
-        {
-            'link': 'https://github.com/HoloPanio',
-            'label': 'Github'
-        },
-        {
-            'link': 'https://twitch.tv/holopaino',
-            'label': 'Twitch'
-        },
-    ],
-}
+const files = fs.readdirSync(`${__dirname}/profiles`);
 
-const erinsantamaria = {
-    'name': 'Erin Santamaria',
-    'image': '/erin.png',
-    'buttons': [
-        {
-            'link': 'https://www.tiktok.com/@recruiter_rin',
-            'label': 'TikTok'
-        },
-        {
-            'link': 'https://www.linkedin.com/in/erin-santamaria-4604a274',
-            'label': 'LinkedIn'
-        },
-    ],
-} 
+let data = {}
 
-const theekrystallee = {
-    'name': 'Krystal Lee',
-    'image': '/krystal.png',
-    'buttons': [
-        {
-            'link': 'https://www.youtube.com/channel/UCXVGYwQXZ18CdlrMsBpQFsw',
-            'label': 'Uncaught Exceptions YouTube'
-        },
-        {
-            'link': 'https://open.spotify.com/episode/4x91OuTcadCUcHtCHfSr0Y',
-            'label': 'Andrew Robles Podcast Inteview'
-        },
-        {
-            'link': 'https://bitclout.com/u/theekrystallee',
-            'label': 'the krystallee coin'
-        },
-        {
-            'link': 'https://www.gofundme.com/c/act/stop-aapi-hate',
-            'label': '#StopAsianHate GoFundMe'
-        },
-        {
-            'link': 'https://linktr.ee/creativeasians',
-            'label': 'Creative Asian Community'
-        },
-        {
-            'link': 'https://docs.google.com/forms/d/e/1FAIpQLSdsU8QGGRQhgwcJy69jMgMobwwDsOOBCvpKnYI6kPq9MLfY9Q/viewform',
-            'label': 'apply to be my simp'
-        },
-        {
-            'link': 'https://discord.com/invite/wYsNTgywF2',
-            'label': 'Creators of TechTok Discord'
-        },
-        {
-            'link': 'https://anchor.fm/uncaughtexceptions',
-            'label': 'Podcast Links'
-        },
-    ],
-}
-
-const data = {
-    'andrewrobles': andrewrobles,
-    'holopanio': holopanio,
-    'erinsantamaria': erinsantamaria,
-    'theekrystallee': theekrystallee
-}
+files.forEach(f => {
+    let fn = f.split('.')[0];
+    data[fn] = require(f);
+})
 
 export default data
