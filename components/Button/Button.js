9124bfdde2a4fe49c9f860f01f0270a1e32570f5
Andrew Robles <me@andrewrobles.com>
2021-04-11 07:40:47 -0700

    Add google icon to button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import styles from './Button.module.css'
 
-export default function Button() {
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-    return <button className={`btn btn-dark btn-block ${styles.Button}`}>Sign up</button>
+export default function Button() {
+    return <button className={`btn btn-dark btn-block ${styles.Button}`}>
+        <FontAwesomeIcon icon={faGoogle} className={`mr-2`} />
+        Sign in with Google
+    </button>
 }
