d6bf8635704a9561dd16f61c85e846b64b22f6ef
Andrew Robles <me@andrewrobles.com>
2021-04-28 04:51:07 -0700

    Enable post request to sign in route

--- backend/src/routes/route.signIn.ts
+++ backend/src/routes/route.signIn.ts
@@ -4,12 +4,12 @@ import { APIResponse } from '../structures/ApiResponse';
 export default [{
 	route: "/signIn",
 	version: "v1",
-	method: "get",
+	method: "post",
 	execute(req: Request, res: Response) {
 		const response = new APIResponse();
 
 		response.setStatus(418);
-		response.setMessage("Profile info");
+		response.setMessage(req.body["name"]);
 		response.setSuccessful();
 
 		res.status(response.status).json(response.compile());
