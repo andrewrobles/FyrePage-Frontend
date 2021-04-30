33ca1bf36447439e1c478b8352ca3bcc2c81d382
Andrew Robles <me@andrewrobles.com>
2021-04-30 04:59:58 -0700

    Define profile model

--- backend/backend/api/models.py
+++ backend/backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    ) 
+    google_id = models.CharField(max_length=100)
+    id_token = models.TextField()
+    
