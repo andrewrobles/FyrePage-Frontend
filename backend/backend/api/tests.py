0e177dcec4e8441a8a8a27529eb744ad7898fb8f
Andrew Robles <me@andrewrobles.com>
2021-04-29 18:42:45 -0700

    Write SignInTestCase.test_new_user

--- backend/backend/api/tests.py
+++ backend/backend/api/tests.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
+from backend.api.models import Profile
+
 class TeapotTestCase(APITestCase):
 
     def test_teapot(self):
@@ -12,3 +14,26 @@ class TeapotTestCase(APITestCase):
         actual_response = self.client.get(url).data
 
         self.assertEqual(expected_response, actual_response)
+
+class SignInTestCase(APITestCase):
+
+    def test_new_user(self):
+        url = reverse('sign-in')
+
+        request_body = {'googleId': 'my-google-id', 'idToken': 'my-id-token'}
+
+        # Create API call to sign in a user that does not exist yet
+        self.client.post(url, request_body, format='json')
+
+        # Verify user exists in database with correct google id  
+        self.assertEqual(1, len(Profile.objects.filter(google_id=request_body['googleId'])))
+
+        # Get user with google id matching request body data
+        new_user = Profile.objects.get(google_id=request_body['googleId'])
+
+        # Verify new user contains correct id token
+        self.assertEqual(request_body['idToken'], new_user.id_token)
+
+
+
+
