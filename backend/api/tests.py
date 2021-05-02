8d91c7b987b6342757ebd661b40882230aa10123
Andrew Robles <me@andrewrobles.com>
2021-05-02 08:09:30 -0700

    Write sign out user test

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,10 +10,11 @@ from django.contrib import auth
 class SignInTestCase(APITestCase):
 
     def setUp(self):
-        self._url = reverse('sign-in')
+        self._sign_in_url = reverse('sign-in')
+        self._sign_out_url = reverse('sign-out')
         self._request_body = {'googleId': 'my-google-id', 'idToken': 'my-id-token'}
 
-    def test_new_profile(self):
+    def test_new_profile_after_first_sign_in(self):
         """
         A new profile should be created if a user signs in for the first time
         """
@@ -30,7 +31,7 @@ class SignInTestCase(APITestCase):
         new_profile = Profile.objects.get(user=new_user)
         self.assertEqual(self._id_token, new_profile.id_token)
 
-    def test_existing_profile(self):
+    def test_existing_profile_after_second_sign_in(self):
         """
         A new profile should not be created if a user has previously signed in
         """
@@ -42,28 +43,49 @@ class SignInTestCase(APITestCase):
         # Verify there is only one profile created
         self.assertEqual(1, len(Profile.objects.all()))
 
-    def test_get_profile_before_sign_in(self):
+    def test_no_profile_access_if_not_signed_in(self):
 
-        response = self.client.get(reverse('profile'), format='json')
+        response = self._get_profile_data()
 
-        self.assertEqual(response.status_code, 403)
+        self._assert_forbidden_response(response)
 
-        self.assertEqual({'detail': 'Authentication credentials were not provided.'}, response.data)
-    
-    def test_get_profile_after_sign_in(self):
+    def test_profile_access_if_signed_in(self):
 
         self._sign_in_user()
 
-        response = self.client.get(reverse('profile'), format='json')
+        response = self._get_profile_data()
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, self._request_body)
+        self._assert_valid_response(response)
+
+    def test_sign_out_user(self):
+
+        self._sign_in_user()
+        self._sign_out_user()
+
+        response = self._get_profile_data()
+
+        self._assert_forbidden_response(response)
+
 
     # Utility functions below this line ---------------------------------------
 
     def _sign_in_user(self):
-        # Create API call to sign in a user
-        self.client.post(self._url, self._request_body, format='json')
+        return self.client.post(self._sign_in_url, self._request_body, format='json')
+
+    def _sign_out_user(self):
+        return self.client.post(self._sign_out_url, self._request_body, format='json')
+
+    def _get_profile_data(self):
+        return self.client.get(reverse('profile'), format='json')
+
+    def _assert_forbidden_response(self, response):
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual({'detail': 'Authentication credentials were not provided.'}, response.data)
+
+    def _assert_valid_response(self, response):
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, self._request_body)
+
 
     @property
     def _google_id(self):
