df0e35f6c7bc41207bc5f8c66d72910ffc71757f
Andrew Robles <me@andrewrobles.com>
2021-05-06 06:31:53 -0700

    Use fetch react hook

--- frontend/components/Landing/Landing.js
+++ frontend/components/Landing/Landing.js
@@ -6,6 +6,7 @@ import Logo from '../Logo/Logo'
 import { useGoogleLogin } from 'react-google-login'
 import { useRouter } from 'next/router'
 
+import { useFetch } from 'react-async'
 
 const clientId = '240083179290-oahd0h3sj4hrd8o0p0i0mf2eqht2re7n.apps.googleusercontent.com'
 
@@ -16,6 +17,12 @@ export default function Landing() {
     const url = 'http://localhost:8000/v1/sign-in/'
     const googleId = 'my-google-id'
     const idToken = 'my-id-token'
+
+    const { data, error } = useFetch(url, {
+      headers: { accept: "application/json" },
+    })
+
+    console.log(`Sign in attempt: ${data}`)
 
     signInUser(url, googleId, idToken).then(response=>{
       console.log(response)
@@ -63,9 +70,9 @@ async function signInUser(url, googleId, idToken) {
     headers: {
       'Content-Type': 'application/json'
     },
-    body: {
+    body: JSON.stringify({
       googleId: googleId,
       idToken: idToken
-    } 
+    })
   })
 }
