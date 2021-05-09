3457164fd6f5d03881621d1b36903b1a7a240d87
Andrew Robles <me@andrewrobles.com>
2021-05-09 10:24:13 -0700

    sign up test helper function

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -5,12 +5,16 @@ from rest_framework.test import APITestCase
 class AuthTestCase(APITestCase):
 
     def test_sign_up(self):
-        url = 'http://localhost:8000/core/users/'
-        data = {'username': 'fyre', 'password': '4321'}
+        expected_username = 'fyre'
 
-        response = self.client.post(url, data, format='json')
+        response = self.sign_up(expected_username, '4321')
 
-        expected_username = data['username']
         actual_username = response.data['username']
 
         self.assertEqual(expected_username, actual_username)
+
+    def sign_up(self, username, password):
+        url = 'http://localhost:8000/core/users/'
+        data = {'username': username, 'password': password}
+
+        return self.client.post(url, data, format='json')
