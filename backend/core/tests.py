d1c8da2bf2ff81adee27e06e031c90c9446f25fe
Andrew Robles <me@andrewrobles.com>
2021-05-09 10:02:26 -0700

    Write sign up test

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+
+class AuthTestCase(APITestCase):
+
+    def test_sign_up(self):
+        url = 'http://localhost:8000/core/users/'
+        data = {'username': 'fyre', 'password': '4321'}
+
+        response = self.client.post(url, data, format='json')
+
+        expected_username = data['username']
+        actual_username = response.data['username']
+
+        self.assertEqual(expected_username, actual_username)
