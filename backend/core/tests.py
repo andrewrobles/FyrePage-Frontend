3fd1f77d48b694279794e74a3baa3377a5ed5731
andrewrobles <andrewroblesdev@gmail.com>
2021-05-09 17:41:39 -0700

    Simplify test

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -7,14 +7,14 @@ class AuthTestCase(APITestCase):
     def test_sign_up(self):
         expected_username = 'fyre'
 
-        response = self.sign_up(expected_username, '4321')
+        response = self.client.post(
+            'http://localhost:8000/core/users/'
+            , {
+                'username': expected_username,
+                'password': '4321'
+            }
+        )
 
         actual_username = response.data['username']
 
         self.assertEqual(expected_username, actual_username)
-
-    def sign_up(self, username, password):
-        url = 'http://localhost:8000/core/users/'
-        data = {'username': username, 'password': password}
-
-        return self.client.post(url, data, format='json')
